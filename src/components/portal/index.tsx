882dba8a79b6bca06730636da1c79c26cef3cbe1
gunessezgi <sezgi.gunes@noyirmibir.com>
2023-07-18 12:40:13 +0300

    commit

--- src/components/portal/index.tsx
+++ src/components/portal/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 
 export type PortalProps = {
@@ -9,10 +9,14 @@ export type PortalProps = {
 /**
  * Easy usage for React Portals
  */
-const Portal = (props: PortalProps) => <>
-    {typeof (window) != "undefined" &&
-        <>{ReactDOM.createPortal(props.children, document.querySelector(props.targetSelector ?? "body") as HTMLElement)}</>
-    }
-</>
+const Portal = (props: PortalProps) => {
+    const [hydrated, setHydrated] = useState(false);
+
+    useEffect(() => {
+        setHydrated(true);
+    }, []);
+
+    return <>{hydrated ? ReactDOM.createPortal(props.children, document.querySelector(props.targetSelector ?? "body") as HTMLElement) : <></>}</>;
+}
 
 export default Portal;
